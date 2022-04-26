5721b9f7fa9a1771fb8db774861bb9f10082b0eb
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-04-26 17:42:05 -0400

    e
    e

---
+++ CodeForces/ProblemSet/WrongSubtraction.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+import java.lang.Math.*;
+
+public class WrongSubtraction {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+
+        int num = sc.nextInt();
+        int reps = sc.nextInt();
+
+        for (int i = 0;l )
+
+		sc.close();
+	}
+}
