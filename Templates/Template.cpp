23fd3f9eb7b846ae79e5eaf83bffe2530746e82b
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-04-27 09:04:24 -0400

    Update Template.cpp

--- Templates/Template.cpp
+++ Templates/Template.cpp
@@ -1,8 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+
+#define FLP(i, a, b) for (int i = a; i <= b; i++)
+
+
 int main() {
 	int rounds;
 	scanf("%d", &rounds);
-	cout >> rounds >> "\n";
 }
