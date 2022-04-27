668178c7c0f53b77654126b0d43e63f99341c475
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-04-27 11:37:13 -0400

    Update WrongSubtraction.java

--- CodeForces/ProblemSet/WrongSubtraction.java
+++ CodeForces/ProblemSet/WrongSubtraction.java
@@ -1,17 +1,26 @@
 import java.io.*;
 import java.util.*;
-import java.lang.*;
 import java.lang.Math.*;
+import java.lang.*;
 
 public class WrongSubtraction {
 	public static void main (String[] args) {
 		Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
 
-        int num = sc.nextInt();
-        int reps = sc.nextInt();
+		int x = sc.nextInt();
+		int rounds = sc.nextInt();
 
-        for (int i = 0;l )
-
+		for (int i = 0; i < rounds; i++) {
+			int last = x%10;
+			if (last != 0) {
+				x--;
+			}
+	
+			if (last == 0) {
+				x /= 10;
+			}
+		}
+		System.out.println(x);
 		sc.close();
 	}
 }
