8118f4093e75a3f4c0edb9b2e613a8a5aacf77ce
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-05-01 14:11:47 -0400

    r
    r

---
+++ CPH/IO.cpp
@@ -0,0 +1,32 @@
+// Input / ouput + Template code
+// c++11 std
+
+// g++-11 -Wall -O2 -std=c++11 %.cpp -o %
+
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+#define ll long long
+#define ld long double
+#define PI 3.1415926535897932384626433832795l
+#define MP make_pair
+#define PB push_back
+#define FLP(i,a,b) for (int i = a; i <= b; i++)
+#define INF (int)1e9
+#define EPS 1e-9
+
+typedef vector<int> VI;
+
+void solve () {
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    return 0;
+
+}
---
+++ CPH/Vector.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+#define ll long long
+#define ld long double
+#define PI 3.1415926535897932384626433832795l
+#define MP make_pair
+#define PB push_back
+#define FLP(i,a,b) for (int i = a; i <= b; i++)
+#define INF (int)1e9
+#define EPS 1e-9
+
+typedef vector<int> VI;
+
+int main() {
+	VI vec;
+	vec.PB(3);
+	vec.PB(2);
+
+
+	for (auto x : vec) {
+		cout << x << "\n";
+	}
+}
+
