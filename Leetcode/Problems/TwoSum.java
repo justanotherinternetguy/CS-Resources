64619057a7aaf4341421dccc82647af7dea1e54b
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-05-02 11:51:11 -0400

    Update TwoSum.java

--- Leetcode/Problems/TwoSum.java
+++ Leetcode/Problems/TwoSum.java
@@ -1,1 +1,22 @@
+import java.util.*;
+// Hashmap implementation
 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        if (nums.length < 2 || nums == null) { // special cases
+            return new int[]{0, 0};
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            }
+            else {
+                map.put(target-nums[i], i);
+            }
+        }
+        return new int[]{0, 0};
+    }
+}
