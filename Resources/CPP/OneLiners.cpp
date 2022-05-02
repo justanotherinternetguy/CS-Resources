f99d886dfd79be38ff4bc6828563b6fb637915a9
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-05-02 13:02:54 -0400

    Create OneLiners.cpp

---
+++ Resources/CPP/OneLiners.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+    // mulitply by 2, 4, 6, 8, etc..
+    int a = 5;
+    a <<= 1; // *2
+    cout << a << "\n"; // 10
+    a <<= 2; // *4
+    cout << a << "\n"; // 40
+    
+    
+    // char upper --> lower
+    char ch = 'B';
+    ch |= ' ';
+    cout << ch << "\n"; // b
+    
+    
+    // check if 32 bit int is power of 2
+    int x = 32; //yes
+    if (x && !(x & (x-1))) {
+        cout << "yes" << "\n";
+    }
+    
+    else {
+        cout << "no" << "\n";
+    }
+    
+    return 0;
+}
