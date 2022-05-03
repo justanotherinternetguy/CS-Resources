e61c0312f363b54c1af4babb3dabf39d582b5efa
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-05-03 11:53:56 -0400

    Update Dictionary.cpp

--- CodeForces/Dictionary.cpp
+++ CodeForces/Dictionary.cpp
@@ -1,42 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
-
-#define all(x) x.begin(), x.end()
-#define FLP(i, a, b) for (int i = a; i <= b; i++)
-
-typedef vector<int> vi;
-
-
 
 int main() {
-	ios_base::sync_with_stdio(0);
+    ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-
-    int rounds;
-    cin >> rounds;
     
-
-    // 1 ab --> 650 zy
-
+    vector<char> ch = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    
+    
+    int rounds;
+    scanf("%d", &rounds);
+    
     for (int i = 0; i < rounds; i++) {
         string s;
-        cin >> s;
-        int res = 0;
-
-        // (int) - 96
-        res += ((int) s[0] - 96);
-        res += ((int) s[1] - 98);
-
-        cout << res << "\n";
-
-
+        cin>>s;
+        
     }
-	return 0;
 }
