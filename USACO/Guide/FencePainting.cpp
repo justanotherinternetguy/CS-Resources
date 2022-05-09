d44e5050295e93dbaff084e010ea33fb88bd31de
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-05-09 10:03:40 -0400

    Create FencePainting.cpp

---
+++ USACO/Guide/FencePainting.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    freopen("paint.in", "r", stdin);
+	freopen("paint.out", "w", stdout);
+	
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    
+    int dist = (b-a) + (d-c);
+    int intersection = max(min(b, d) - max(a, c), 0);
+    int res = dist - intersection;
+    cout << res << "\n";
+}
