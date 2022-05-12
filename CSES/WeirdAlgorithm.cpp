2c74723970157c4127901d60706b95b75c5c8d9d
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-05-12 11:40:58 -0400

    Update WeirdAlgorithm.cpp

--- CSES/WeirdAlgorithm.cpp
+++ CSES/WeirdAlgorithm.cpp
@@ -3,13 +3,20 @@ using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL);
-	int n {};
+	long long n {};
 	cin >> n;
 
-	do {
-		if (n % 2 == 0) { n /= 2; }
-		else if (n % 2 == 1) { n = (n*3)+1; }
-		cout << n << '\n';
-	} while (n != 1);
+	if (n == 1) { cout << n; }
+
+	else {
+		do {
+			cout << n << ' ';
+			if (n % 2 == 0) { n /= 2; }
+			else if (n % 2 == 1) { n = (n*3)+1; }
+		} while (n != 1);
+		cout << n;
+	}
+
+
 	
 }
