32b8a57ec7f859c29389a7cfde519a324d5a0c98
justanotherinternetguy <hendryxu1@gmail.com>
2022-11-16 20:34:57 -0500

    e

---
+++ USACO/Guide/shell_game.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	freopen("shell.in", "r", stdin);
+	freopen("shell.out", "w", stdout);
+
+
+    int N;
+    cin >> N;
+
+
+
+    return 0;
+}
