887b27b6419c44d393556aa3f535594fe9225e45
justanotherinternetguy <hendryxu1@gmail.com>
2022-12-20 22:07:26 -0500

    e

--- USACO/22-23/other.cpp
+++ USACO/22-23/other.cpp
@@ -1,67 +1,7 @@
-#include <iostream>
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-const int MAXN = 105;
-
-int t, n, m;
-unordered_map<string, int> mp;
-
-bool check(int idx, int val) {
-    if (idx == n) {
-        return true;
-    }
-
-    for (auto [s, x] : mp) {
-        if (s[idx] == '1' && x != val) {
-            return false;
-        }
-        if (s[idx] == '0' && x == val) {
-            return false;
-        }
-    }
-
-    return check(idx + 1, val) || check(idx + 1, val ^ 1);
-}
-
 int main() {
-    cin >> t;
-
-    while (t--) {
-        cin >> n >> m;
-        mp.clear();
-
-        bool lie = false;
-        for (int i = 0; i < m; i++) {
-            string s;
-            cin >> s;
-            int x;
-            cin >> x;
-
-            mp[s] = x;
-        }
-
-        bool ok = false;
-        for (int i = 0; i < (1 << n); i++) {
-            if (check(0, i)) {
-                ok = true;
-                break;
-            }
-        }
-
-        if (!ok) {
-            lie = true;
-        }
-
-        if (lie) {
-            cout << "LIE" << endl;
-        } else {
-            cout << "OK" << endl;
-        }
-    }
-
+    cout << "HI" << '\n';
     return 0;
 }
