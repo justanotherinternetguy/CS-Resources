f8c26b856abe116fe0186cf7dca5e3fab8be6828
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2023-01-09 13:18:53 -0500

    Create template.cpp

---
+++ USACO/22-23/template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+const int INF = 1e9;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  freopen("paint.in", "r", stdin);
+  freopen("paint.out", "w", stdout);
+
+  return 0;
+}
