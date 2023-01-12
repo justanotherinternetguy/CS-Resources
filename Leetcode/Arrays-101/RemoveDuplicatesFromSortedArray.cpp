4dbccae07856085a6735a6a10a38b8e8a7b4156b
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2023-01-12 14:00:33 -0500

    Update RemoveDuplicatesFromSortedArray.cpp

--- Leetcode/Arrays-101/RemoveDuplicatesFromSortedArray.cpp
+++ Leetcode/Arrays-101/RemoveDuplicatesFromSortedArray.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.size() <= 1) {
-            return nums.size();
-        }
-
-        int i = 0;
-
-        for (int j = 1; j < nums.size(); j++) {
-            if (nums[j] != nums[i]) {
-                i++;
-                nums[i] = nums[j];
+        int k = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            //bypass
+            if (i < nums.size() - 1 && nums[i] == nums[i+1]) {
+                continue;
             }
+            nums[k] = nums[i];
+            k++;
         }
-
-        return i + 1;
+        return k;
+        
     }
 };
