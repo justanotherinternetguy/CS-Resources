925eac10e54d75af8523d671563e700dbc47b103
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2023-01-30 22:04:33 -0500

    e

---
+++ USACO/22-23/bovineshuffle.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long;
+
+
+int main() {
+  freopen("shuffle.in", "r", stdin);
+  freopen("shuffle.out", "w", stdout);
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  int pos[n];
+  int ids[n];
+
+  int ans[n];
+
+  for (int i = 0; i < n; i++) {
+    cin >> pos[i];
+  }
+
+  for (int i = 0; i < n; i++) {
+    cin >> ids[i];
+  }
+
+
+  
+  
+  return 0;
+}
