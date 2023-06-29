28fb9bd171db7a11a95b8a19f1055836fccc4233
justanotherinternetguy <mithrandir>
2023-06-28 21:39:31 -0400

    e

---
+++ USACO/NEW/test.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  freopen("./shuffle.in", "r", stdin);
+	freopen("./shuffle.out", "w", stdout);
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  return 0;
+}
